0414f6b8cbd4b8736af9c4019baf1b403741ec26
Murray Ataga <ataga@Murrays-MacBook-Pro.local>
2025-03-12 17:47:52 +0100

    initial commit

---
+++ src/test/java/Base.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class Base {
+
+    @Test
+    public void Configure(){
+        System.out.println("Testing appium");
+    }
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class test {
+
+
+    @Test
+    public void runTest(){
+
+        System.out.println("checking tests");
+    }
+
+
+}
